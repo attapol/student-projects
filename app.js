61528f816dc828252da2d9f97673308546e06653
Te Rutherford <attapolrutherford@gmail.com>
2022-12-27 00:18:40 +0700

    Added images and changed filtering logic

--- app.js
+++ app.js
@@ -1,56 +1,140 @@
-const projects = [
-  {
-    "name": "Nifty Wozniak",
-    "kind": "Kind X",
-    "year": "2020",
-    "image": "images/0.png",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend purus nec commodo semper. Phasellus eget lacinia ex. Mauris ultrices dui eros, in auctor nunc venenatis tristique. Phasellus gravida diam nunc, vel lacinia magna ultricies sit amet. Sed lacus dui, egestas lacinia efficitur in, cursus vel libero. Suspendisse luctus, tellus sed consectetur dignissim, libero est tincidunt orci, id tristique tortor dui sit amet leo. Vivamus sed ante vel lorem congue euismod. Cras commodo et lacus eget finibus. Proin gravida ultricies malesuada. Nulla aliquam vehicula leo non pulvinar."
-  },
-  {
-    "name": "Flamboyant Herschel",
-    "kind": "Kind Y",
-    "year": "2020",
-    "image": "images/1.png",
-    "description": "Quisque imperdiet purus id nunc congue varius. Suspendisse non arcu sapien. Cras vitae mauris vel quam volutpat eleifend. Vivamus ac est sed erat cursus interdum a vel nisi. Morbi sit amet pellentesque lectus. Fusce laoreet sapien vel bibendum tempor. Vivamus aliquam hendrerit metus, a vehicula lacus dignissim vel."
-  },
-  {
-    "name": "Sleepy Visvesvaraya",
-    "kind": "Kind Z",
-    "year": "2020",
-    "image": "images/2.png",
-    "description": "Proin maximus lacus eros, et tincidunt ante scelerisque et. Cras a ornare magna. Nullam quis gravida turpis. Curabitur et tellus eu quam vulputate vulputate. Nunc scelerisque efficitur consequat. Pellentesque ac urna diam. Aenean vel mattis mauris, ut eleifend neque. In sit amet orci eu urna vulputate suscipit nec non ante. Curabitur malesuada tincidunt magna, ut pharetra lacus. Donec non aliquam dui. Sed a nibh sagittis, sollicitudin leo et, ullamcorper mi. Nunc volutpat metus nisi, in imperdiet massa volutpat non."
-  },
-  {
-    "name": "Elastic McNulty",
-    "kind": "Kind X",
-    "year": "2021",
-    "image": "images/3.png",
-    "description": "Nullam mauris sem, venenatis ac dui eget, dignissim consequat justo. Pellentesque nec nisl efficitur, ornare ipsum at, pharetra mi. Aliquam a faucibus tortor, in condimentum quam. Suspendisse sit amet lorem egestas sapien pellentesque varius lobortis ac risus. Mauris ut efficitur nunc, in dignissim turpis. Fusce eget lorem in lacus dignissim aliquam at et nibh. In in ante suscipit leo tristique semper. Curabitur at augue congue, iaculis ante sed, tristique ex. Cras sollicitudin nibh a mauris venenatis facilisis. Duis a malesuada purus. In hac habitasse platea dictumst. Suspendisse bibendum mauris non quam accumsan, a finibus metus auctor. Quisque libero purus, imperdiet vel pretium a, sollicitudin at ligula."
-  },
-  {
-    "name": "Awesome Villani",
-    "kind": "Kind Y",
-    "year": "2021",
-    "image": "images/4.png",
-    "description": "Morbi et elementum tellus, eu vehicula nulla. Duis congue, arcu et finibus condimentum, nisi ex facilisis lorem, eu tempus arcu dolor nec enim. Pellentesque dignissim sodales nibh mattis commodo. Curabitur blandit feugiat blandit. Aenean lobortis nulla felis, vitae blandit turpis facilisis vel. Vivamus ipsum augue, posuere id dapibus ut, iaculis at nulla. Mauris at aliquet ex. Aliquam vel sollicitudin velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin eu lacinia dolor. Nam in venenatis nibh."
-  },
-  {
-    "name": "Peaceful Black",
-    "kind": "Kind Z",
-    "year": "2021",
-    "image": "images/5.png",
-    "description": "Ut varius risus lectus, vel venenatis nulla facilisis non. Curabitur mollis libero quis lectus commodo tempus. Vestibulum elementum velit non mauris dictum, eget commodo eros iaculis. Phasellus ac purus quis nulla faucibus tincidunt quis eu sem. Etiam sed eros et velit posuere pharetra. Etiam non blandit urna. Cras risus mi, condimentum eu odio eu, elementum molestie ligula. Suspendisse magna neque, mollis vel fringilla eu, vehicula nec elit. Cras nec mauris vulputate, sodales tellus quis, venenatis purus. Curabitur nec metus velit."
+const projects = [{'Title': 'การศึกษาหนังสือสอนภาษาไทยสำหรับผู้เรียนชาวต่างชาติของ CTFL (Center For Thai As A Foreign Language) เทียบกับมาตรฐาน CEFR (Common European Framework of Reference for Languages)',
+'Name': 'รินทราย พงษ์วะสา',
+'Advisor': 'ผศ.ดร.ณัฏฐนันท์ จันทร์เจ้าฉาย',
+'image': 'images/rinsai.png',
+'year': 2021},
+{'Title': 'Early Sentence: Language Development of 21- to 24- month-old Thai Children',
+'Name': 'ฝนทิพย์ เชื้อสง่า',
+'Advisor': 'ผศ.ดร.ณัฏฐนันท์ จันทร์เจ้าฉาย',
+'image': 'images/fonthip.png',
+'year': 2021},
+{'Title': 'การเปรียบเทียบวงคำศัพท์ในหนังสือนิทานเด็กปฐมวัยกับหนังสือเรียนวิชาภาษาไทย ระดับชั้นประถมศึกษา ช่วงชั้นที่ 1',
+'Name': 'สุดาภัทร ฉัตรกวีกุล',
+'Advisor': 'ผศ.ดร.ณัฏฐนันท์ จันทร์เจ้าฉาย',
+'image': 'images/sudaphat.png',
+'year': 2021},
+{'Title': 'อนุภาคท้ายระบุความสุภาพและสรรพนามบุรุษที่หนึ่งที่เป็นกลางทางเพศในภาษาไทย',
+'Name': 'ธิติวุฒิ โสมมูล',
+'Advisor': 'ผศ.ดร.ภาวดี สายสุวรรณ',
+'image': 'images/thitivut.png',
+'year': 2021},
+{'Title': 'การศึกษาคุณลักษณะทางกลสัทศาสตร์ของการปลอมแปลงเสียงผ่านค่าที่เกี่ยวข้องกับระยะเวลา',
+'Name': 'กัณฐิกา อภิญโญวิเชียร',
+'Advisor': 'ผศ.ดร.ศุจิณัฐ จิตวิริยนนท์',
+'image': '',
+'year': 2021},
+{'Title': 'การศึกษาค่าทางกลสัทศาสตร์ของพยัญชนะกักภาษาเกาหลีที่ออกเสียงโดยผู้พูดภาษาไทยเป็นภาษาแม่เพื่อการพัฒนาสื่อการสอนแบบโสตทัศนวิธี',
+'Name': 'กฤษฎา แพทย์เจริญ',
+'Advisor': 'ผศ.ดร.ศุจิณัฐ จิตวิริยนนท์',
+'image': 'images/bank.png',
+'year': 2021},
+{'Title': 'การตัดคำภาษาไทยจากเอกสารรายงานการประชุมของคณะกรรมการธนาคารแห่งประเทศไทย',
+'Name': 'กานต์ชนิต พิศพันธุ์',
+'Advisor': 'รศ.ดร.ทรงพันธ์ เจิมประยงค์',
+'image': 'images/pang.png',
+'year': 2021},
+{'Title': 'ระบบแจ้งของหายภายในจุฬาลงกรณ์มหาวิทยาลัย (CU LOST&FOUND Management System)',
+'Name': 'กมลพรรณ เลิศวิลัย',
+'Advisor': 'รศ.ดร.ทรงพันธ์ เจิมประยงค์',
+'image': '',
+'year': 2021},
+{'Title': 'Analyzing Thailand’s Sandbox through SHA Hotels using Microsoft Power BI',
+'Name': 'กมลกร เบื้องสูง',
+'Advisor': 'รศ.ดร.ทรงพันธ์ เจิมประยงค์',
+'image': '',
+'year': 2021},
+{'Title': 'การตรวจจับข้อมูลเท็จในข่าวหัวข้อโควิด-19 ด้วยการเรียนรู้ด้วยเครื่อง',
+'Name': 'จิดาภา อักษรสมัย',
+'Advisor': 'รศ.ดร.ทรงพันธ์ เจิมประยงค์',
+'image': '',
+'year': 2021},
+{'Title': 'การประเมินข้อมูลที่ควรอ้างอิงบนวิกิพีเดียภาษาไทย : Wikipedia Citation Recommendation',
+'Name': 'ณัฐนันท์ สุทธนู',
+'Advisor': 'รศ.ดร.ทรงพันธ์ เจิมประยงค์',
+'image': '',
+'year': 2021},
+{'Title': 'On the acoustic reality of “toneless” morphemes in Thai: Preliminary investigations through sequence-internal final particles',
+'Name': 'รฐนนท์ เจียมสันดุษฎี',
+'Advisor': 'รศ.ดร.พิทยาวัฒน์ พิทยาภรณ์',
+'image': 'images/ratanont.png',
+'year': 2021},
+{'Title': 'ทดลองสร้างธีซอรัสคำพ้องความภาษาไทย',
+'Name': 'ธวัลหทัย ไมตรีจิต',
+'Advisor': 'รศ.ดร.วิโรจน์ อรุณมานะกุล',
+'image': '',
+'year': 2021},
+{'Title': 'การพัฒนาเว็บแอปพลิเคชันเรียนภาษาญี่ปุ่นผ่านเนื้อเพลง',
+'Name': 'ญาธิป เจริญวราวุฒิ',
+'Advisor': 'รศ.ดร.วิโรจน์ อรุณมานะกุล',
+'image': 'images/yathip.png',
+'year': 2021},
+{'Title': 'การเปรียบเทียบการวิเคราะห์ความรู้สึกในภาษาไทยด้วยเทคนิค  Naïve Bayes และ Logistic Regression จากข้อมูลทวิตเตอร์หัวข้อแอปพลิเคชันสั่งอาหาร',
+'Name': 'พัชรวลัย อนุเทียนชัย',
+'Advisor': 'รศ.ดร.วิโรจน์ อรุณมานะกุล',
+'image': '',
+'year': 2021},
+{'Title': 'การพัฒนาโมเดลเพื่อตัดแบ่งข้อความภาษาไทย: กรณีศึกษาจากรายงานการประชุมของธนาคารแห่งประเทศไทย',
+'Name': 'สุปรียาทร อภัยสุวรรณ',
+'Advisor': 'รศ.ดร.วิโรจน์ อรุณมานะกุล',
+'image': '',
+'year': 2021},
+{'Title': 'โครงการระบบวิเคราะห์ความคิดเห็นข้อความบนTwitterเกี่ยวกับการซื้อขายออนไลน์',
+'Name': 'ธวัลรัตน์ อรุณเรื่อ',
+'Advisor': 'รศ.ดร.วิโรจน์ อรุณมานะกุล',
+'image': 'images/thawanrat.png',
+'year': 2021},
+{'Title': 'การวิเคราะห์ข้อเสนอแนะของผู้แสดงความคิดเห็นในโครงการแก้ไขเพิ่มเติม พระราชบัญญัติกองทุนเงินให้กู้ยืมเพื่อการศึกษา พ.ศ. 2560',
+'Name': 'ศดานันท์ อาศัยบุญ',
+'Advisor': 'รศ.ดร.อรรถพล ธำรงรัตนฤทธิ์',
+'image': 'images/sadanan.png',
+'year': 2021},
+{'Title': 'เหนือกว่าเวิร์ดคลาวด์: การแสดงตัวบทให้เป็นภาพเชิงความหมาย',
+'Name': 'เกษราภรณ์ ฤาชัย',
+'Advisor': 'รศ.ดร.อรรถพล ธำรงรัตนฤทธิ์',
+'image': 'images/kedsaraporn.png',
+'year': 2021},
+{'Title': 'Long live the king: The indication of moods by emojis and the correlation of moods with Thai political crisis in 2019-2020',
+'Name': 'ภวิศภาคย์ อัครจรัสวงศ์',
+'Advisor': 'รศ.ดร.อรรถพล ธำรงรัตนฤทธิ์',
+'image': 'images/paolong.png',
+'year': 2021},
+{'Title': 'การคัดเลือกทวีตที่เป็นประโยชน์ต่อผู้ชุมนุมประท้วงเมื่อวันที่ 16 ตุลาคม 2563 และวันที่ 14 พฤศจิกายน 2564 ด้วยเครื่อง',
+'Name': 'ปวีณ มานะกุล',
+'Advisor': 'รศ.ดร.อรรถพล ธำรงรัตนฤทธิ์',
+'image': '',
+'year': 2021},
+{'Title': 'การวิเคราะห์เนื้อหาที่ปรากฏภายใต้แฮชแท็ก #สมรสเท่าเทียม ในสื่อสังคมทวิตเตอร์ (Twitter)',
+'Name': 'สุพิชฌาย์ เปี่ยมนิติกร',
+'Advisor': 'ผศ.ดร.วชิราภรณ์ คลังธนบูรณ์',
+'image': 'images/supitcha.png',
+'year': 2021},
+{'Title': 'การวิเคราะห์เนื้อหาการจัดการข้อมูลวิจัยในเว็บไซต์ห้องสมุดของมหาวิทยาลัย',
+'Name': 'กนกลดา สุหร่ายเพชร',
+'Advisor': 'ผศ.ดร.วชิราภรณ์ คลังธนบูรณ์',
+'image': '',
+'year': 2021},
+{'Title': ' การศึกษาการออกแบบเว็บไซต์หอจดหมายเหตุ:  กรณีศึกษาเว็บไซต์หอจดหมายเหตุสถาบันศึกษาในไทย',
+'Name': 'อรสินี อ่อนชิต',
+'Advisor': 'ผศ.ดร.วชิราภรณ์ คลังธนบูรณ์',
+'image': '',
+'year': 2021}]
+;
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
-];
+shuffle(projects)
 
 const render = () => {
   var container = document.getElementById("root");
 
   const categories = {
-    kind: new Set(projects.map(p => p.kind)),
+    advisor: new Set(projects.map(p => p.Advisor)),
     year: new Set(projects.map(p => p.year)),
   };
-  const filters = { kind: new Set(), year: new Set() };
+  const filters = { advisor: new Set(), year: new Set() };
 
   const buttonID = (key, value) => [key, value].join(" ").replaceAll(" ", "-");
   const projectID = i => `project-${i}`;
@@ -82,15 +166,30 @@ const render = () => {
     return row;
   }
 
+  function turnOffAllButtons(category) {
+    const values = categories[category]
+    values.forEach( value => {
+        const button = document.getElementById(buttonID(category,value));
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-secondary");
+        if (filters[category].has(value)) {
+            filters[category].delete(value);
+        }
+    });
+
+  }
+
   function toggleButton(key, value) {
-    const id = buttonID(key, value);
-    const button = document.getElementById(id);
-    const turnOff = filters[key].has(value);
+    const turnOn = !filters[key].has(value);
+    turnOffAllButtons(key)
+    if (turnOn) { 
+        const id = buttonID(key, value);
+        const button = document.getElementById(id);
+        filters[key].add(value);
+        button.classList.add("btn-primary")
+        button.classList.remove("btn-secondary");
 
-    turnOff ? filters[key].delete(value) : filters[key].add(value);
-    button.classList.remove( turnOff ? "btn-primary" : "btn-secondary" );
-    button.classList.add( turnOff ? "btn-secondary" : "btn-primary" );
-
+    }
     toggleAllButton(false);
     showOrHideProjects();
   }
@@ -99,25 +198,30 @@ const render = () => {
     const button = document.getElementById("all-values");
 
     if (clicked) {
-      filters.kind.forEach( value => toggleButton("kind", value) );
-      filters.year.forEach( value => toggleButton("year", value) );
-      filters.kind.clear();
+      filters.advisor.clear();
       filters.year.clear();
+      turnOffAllButtons('advisor');
+      turnOffAllButtons('year');
+      button.classList.remove("btn-secondary");
+      button.classList.add("btn-primary");
+      showOrHideProjects();
+    } else if (filters.advisor.size === 0 && filters.year.size === 0){
+      button.classList.remove("btn-secondary");
+      button.classList.add("btn-primary");
+    } else {
+      button.classList.add("btn-secondary");
+      button.classList.remove("btn-primary");
     }
 
-    button.classList.add( clicked ? "btn-primary" : "btn-secondary" );
-    button.classList.remove( clicked ? "btn-secondary" : "btn-primary" );
 
-    showOrHideProjects();
   }
 
   function showOrHideProjects() {
     projCards = document.getElementById("project-cards")
-    //projCards.innerHTML = '';
 
     projects.forEach( (project, i) => {
       const visible = (
-        (filters.kind.size === 0 || filters.kind.has(project.kind)) &&
+        (filters.advisor.size === 0 || filters.advisor.has(project.Advisor)) &&
         (filters.year.size === 0 || filters.year.has(project.year))
       );
       const card = document.getElementById(projectID(i));
@@ -131,8 +235,8 @@ const render = () => {
   }
 
   function renderButtons() {
-    // container.appendChild( addAllButton() );
-    //buttonRows = document.getElementById("buttons").appendChild( addAllButton());
+    const allButton = document.getElementById("all-values");
+    allButton.onclick = () => toggleAllButton(true);
     const buttonRows = document.getElementById("buttons")
     Object.entries(categories).map( ([category, values]) => {
       const row = document.createElement("div");
@@ -147,9 +251,14 @@ const render = () => {
   function makeCardImage(project) {
 
     const image = document.createElement("img");
-    image.setAttribute("class", "card-img-top ")
-    image.src = project.image;
-    image.alt = project.name;
+    image.setAttribute("class", "card-img-top card-image")
+    image.setAttribute("data-src","holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail")
+    if (project.image == ""){
+        image.src = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22508%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20508%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1854eff71ba%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1854eff71ba%22%3E%3Crect%20width%3D%22508%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22169.75390625%22%20y%3D%22123.6%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+    }else {
+        image.src = project.image;
+    }
+    image.alt = project.Title;
     return image;
   }
 
@@ -157,30 +266,31 @@ const render = () => {
     const cardBody = document.createElement("div");
     cardBody.setAttribute("class", "card-body");
 
-    function addCardElement(kind, content) {
-      const element = document.createElement(kind);
+    function addCardElement(advisor, content) {
+      const element = document.createElement(advisor);
       element.innerHTML = content;
       cardBody.appendChild(element);
     }
 
-    addCardElement("h3", project.name);
-    addCardElement("h4", project.kind);
-    addCardElement("h4", project.year);
-    const p = document.createElement("p")
-    p.setAttribute("class", "card-text")
-    p.innerHTML = project.description
-    cardBody.appendChild(p)
+    addCardElement("h5", project.Title);
+    addCardElement("p", project.Name);
+    addCardElement("p", project.Advisor);
+    //const p = document.createElement("p")
+    //p.setAttribute("class", "card-text")
+    //p.innerHTML = project.description
+    //cardBody.appendChild(p)
     return cardBody;
   }
 
   function addCard(project, i) {
     var project = projects[i];
-    var col = document.createElement("col")
-    var card = document.createElement("div");
+    var col = document.createElement("div")
+    col.classList.add("col-md-4")
     col.setAttribute("id", projectID(i));
     col.classList.add("fade", "show")
-
-    card.setAttribute("class", "card shadow-sm");
+    var card = document.createElement("div");
+    card.setAttribute("class", "card mb-4 shadow-sm");
+    //card.clas
 
     card.appendChild( makeCardImage(project) );
     card.appendChild( makeCardBody(project) );
